df11b51067a83600fe36bcc7791008a632f3252f
JuanAntonioRojas <tonyrojas007@gmail.com>
2025-09-15 20:57:56 -0400

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lms {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.ims to javafx.fxml;
-    exports com.example.ims;
-}
